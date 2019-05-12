5af2adc39ac3c2a206463ac8e6444b05bc695b56
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2019-05-11 20:43:23 -0400

    Gave every point its own rectangle

--- drawPoints.js
+++ drawPoints.js
@@ -6,7 +6,7 @@ var dataset = [];
 var xm = d3.scale.linear().domain([0, w]).range([0, w]);
 var ym = d3.scale.linear().domain([0, h]).range([0, h]);
 var pts = { cx: function (d) { return xm(d.x); }, cy: function (d) { return ym(d.y); }, r: radius };
-//add point
+
 svg.on("click", function () {
       var xy = d3.mouse(this);
       var pt = { x: Math.round(xm.invert(xy[0])), y: Math.round(ym.invert(xy[1])) };
@@ -21,12 +21,11 @@ svg.on("click", function () {
       console.log(pt);
 })
 
+// Drawing boundary around the frame
 drawBoundary(0, 0, 0, h);
 drawBoundary(0, 0, w, 0);
 drawBoundary(0, h, w, h);
 drawBoundary(w, 0, w, h);
-
-console.log("w = " + w + "; h = " + h);
 
 function over(d, i) {
       d3.select(this).attr({ fill: "red", r: radius * 1.5 });
@@ -36,20 +35,33 @@ function out(d, i) {
       d3.select(this).attr({ fill: "black", r: radius });
 }
 
-function test() {
-      console.log("hello");
-}
+var setRectangles = [];
 
 function kdDriver() {
+      setRectangles = []
       var pointSet = new Array(dataset.length);
       var index = 0;
-      for (points of dataset) {
-            pointSet[index++] = [points.x, points.y];
+      for (point of dataset) {
+            pointSet[index++] = [point.x, point.y];
+            var setRectangle = { x: point.x, y: point.y, up: 0, left: 0, down: h, right: w };
+            setRectangles.push(setRectangle);
       }
       var verticalMedians = [0, w]
       var horizontalMedians = [0, h]
+      console.log(setRectangles);
       kdAlgo(pointSet, 1, 1, h, verticalMedians, horizontalMedians);
+      console.log(setRectangles);
+      drawRectangles();
 }
+
+function updateValue(x, y, boundaryType, boundaryVal) {
+      for (point of setRectangles) {
+            if (point.x == x && point.y == y) {
+                  point[boundaryType] = boundaryVal;
+            }
+      }
+}
+
 // UP and LEFT is true, rest false
 function kdAlgo(pointSet, takeXMedian, isUpOrLeft, oldMedian, verticalMedians, horizontalMedians) {
       console.log(verticalMedians);
@@ -60,7 +72,7 @@ function kdAlgo(pointSet, takeXMedian, isUpOrLeft, oldMedian, verticalMedians, h
 
       var verticalMediansCopy = verticalMedians.slice();
       var horizontalMediansCopy = horizontalMedians.slice();
-      
+
       if (takeXMedian) {
             var xPoints = getPoints(pointSet, 0); // Extract X Points
             medianValue = getMedian(xPoints);
@@ -69,15 +81,16 @@ function kdAlgo(pointSet, takeXMedian, isUpOrLeft, oldMedian, verticalMedians, h
             for (var point of pointSet) {
                   if (point[0] < medianValue) {
                         leftPointSet.push(point);
+                        updateValue(point[0], point[1], "right", medianValue);
                   }
                   else {
                         rightPointSet.push(point);
+                        updateValue(point[0], point[1], "left", medianValue);
                   }
             }
-            drawMedian(medianValue, 1, isUpOrLeft, oldMedian, horizontalMedians); // Plot Median Line
             verticalMediansCopy.push(medianValue);
             verticalMediansCopy = verticalMediansCopy.sort(function (a, b) {
-                  return a - b; 
+                  return a - b;
             });
             console.log("left: " + leftPointSet.length + " right: " + rightPointSet.length);
             var nextSplit = !takeXMedian;
@@ -92,68 +105,38 @@ function kdAlgo(pointSet, takeXMedian, isUpOrLeft, oldMedian, verticalMedians, h
             for (var point of pointSet) {
                   if (point[1] < medianValue) {
                         upPointSet.push(point);
+                        updateValue(point[0], point[1], "down", medianValue);
                   }
                   else {
                         downPointSet.push(point);
+                        updateValue(point[0], point[1], "up", medianValue);
                   }
             }
-            drawMedian(medianValue, 0, isUpOrLeft, oldMedian, verticalMedians); // Plot Median Line
             horizontalMediansCopy.push(medianValue);
             horizontalMediansCopy = horizontalMediansCopy.sort(function (a, b) {
-                  return a - b; 
+                  return a - b;
             });
             console.log("up: " + upPointSet.length + " down: " + downPointSet.length);
             var nextSplit = !takeXMedian;
             kdAlgo(upPointSet, nextSplit, 1, medianValue, verticalMedians, horizontalMediansCopy);
             kdAlgo(downPointSet, nextSplit, 0, medianValue, verticalMedians, horizontalMediansCopy);
       }
-      return; 
+      return;
 }
 
-function drawMedian(medianValue, isVertical, isUpOrLeft, oldMedian, medianList) {
-      var x1, x2, y1, y2;
-      if (isVertical) {
-            x1 = medianValue;
-            x2 = medianValue;
-            y1 = oldMedian;
-            y2 = h;
-            if (isUpOrLeft) {
-                  y2 = medianList.sort(function (a, b) {
-                        return b - a; 
-                  }).find(function(element) {
-                        return element < oldMedian;
-                      });
-            } else {
-                  y2 = medianList.find(function(element) {
-                        return element > oldMedian;
-                      });
-            }
+function drawRectangles() {
+      console.log("Drawing rectangles...");
+      for (rectangle of setRectangles) {
+            svg.append("rect")
+                  .attr("x", rectangle.left)
+                  .attr("y", rectangle.up)
+                  .attr("width", rectangle.right - rectangle.left)
+                  .attr("height", rectangle.down - rectangle.up)
+                  .attr("fill-opacity", 0.1)
+                  .attr("fill", "black")
+                  .attr("stroke-width", 2)
+                  .attr("stroke", "black");;
       }
-      else {
-            y1 = medianValue;
-            y2 = medianValue;
-            x1 = oldMedian;
-            x2 = w; 
-            if (isUpOrLeft) {
-                  x2 = medianList.sort(function (a, b) {
-                        return b - a; 
-                  }).find(function(element) {
-                        return element < oldMedian;
-                      });
-            }
-            else {
-                  x2 = medianList.find(function(element) {
-                        return element > oldMedian;
-                      });
-            }
-      }
-      svg.append("line")
-            .attr("x1", x1)
-            .attr("y1", y1)
-            .attr("x2", x2)
-            .attr("y2", y2)
-            .attr("stroke-width", 2)
-            .attr("stroke", "black");
 }
 
 function getPoints(pointSet, index) {
@@ -168,7 +151,7 @@ function getPoints(pointSet, index) {
 
 function getMedian(points) {
       points = points.sort(function (a, b) {
-            return a - b; 
+            return a - b;
       });
       // console.log("Sorted points: " + points);
       var half = Math.floor(points.length / 2);
@@ -184,4 +167,8 @@ function drawBoundary(x1, y1, x2, y2) {
             .attr("y2", y2)
             .attr("stroke-width", 2)
             .attr("stroke", "black");
+}
+
+function randomizePoints() {
+
 }
